432b8531f871b9c803b9671f4038e58c4d9a759f
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2020-05-01 14:53:22 -0500

    Process

--- Process.py
+++ Process.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import glob, os
+import xml.etree.ElementTree as ET
 
 PATH='/home/quantics/public_html/static/'
 os.chdir(PATH)
 
-for file in glob.glob("*0.xml"):
-    print(file,"locs")
+for file in glob.glob(PATH+"*0.xml"):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    print(root)
+
 
 for file in glob.glob("*1.xml"):
-    print(file,"prices")
