c65a61cb348abb1591c136f0d8ef6b0a88723030
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2020-05-01 14:35:49 -0500

    Process

--- Process.py
+++ Process.py
@@ -2,7 +2,7 @@
 import glob, os
 
 PATH='/home/quantics/public_html/static/'
-os.chdir(PATH+"backup/")
+os.chdir("/home/quantics/public_html/static/backup/")
 
 for file in glob.glob("*0.xml"):
     print(file,"locs")
