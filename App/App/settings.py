53047f8adeb73bd3868e066a18208e01015a7f19
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-02-08 14:21:50 -0600

    App urls

--- App/App/settings.py
+++ App/App/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+TIME_ZONE = 'America/Mexico_City'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'leaflet',
+     'mezzanine_gasStation_map',
 ]
 
 MIDDLEWARE = [
--- App/App/urls.py
+++ App/App/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mezzanine_gasStation_map.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('map/', include('mezzanine_gasStation_map.urls')),
 ]
