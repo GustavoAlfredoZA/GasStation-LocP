4ee1463280e2563927a13d75bd7df3aa94b1828c
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-02-11 11:26:35 -0600

    Process StoreDB Postgresql

--- ProcessPostgresql.py
+++ ProcessPostgresql.py
@@ -1,19 +1,10 @@
 import json
-import mysql.connector
-from mysql.connector import errorcode
 import subprocess
-
-p = open("routes.json")
-routes = json.load(p)
-ProyectPATH=routes['proyect']
-PublicPATH=routes['public']
-p.close()
-
-with open('db.json') as json_file:
-    config = json.load(json_file)
+import psycopg2
+import os
 
 try:
-    cnx = mysql.connector.connect(**config)
+    cnx = psycopg2.connect(os.getenv('DATABASE_URL', 'Optional default value'))
     cursor = cnx.cursor()
     updatestates = "INSERT INTO pricesTime(statec,number,datec,priceregular,pricepremium,pricediesel,nregular,npremium,ndiesel) SELECT tmp.state, tmp.numberplaces ,tmp. today, tmp.pricer, tmp.pricep, tmp.pricep, tmp.numberr, tmp.numberp, tmp.numberd FROM ( SELECT places.state AS state, COUNT(*) AS numberplaces, CURDATE() AS today, SUM(regular) AS pricer, SUM(premium) AS pricep, SUM(diesel) AS priced, COUNT(regular) AS numberr, COUNT(premium) AS numberp, COUNT(diesel) AS numberd FROM places LEFT JOIN prices ON places.place_id = prices.prices_place_id WHERE state is not null GROUP BY places.state ) AS tmp ON DUPLICATE KEY UPDATE priceregular = tmp.pricer, pricepremium = tmp.pricep, pricediesel = tmp.priced, nregular = tmp.numberr, npremium = tmp.numberp, ndiesel = tmp.numberd"
     query=("SELECT places.place_id,places.name,places.cre_id,places.x,places.y,prices.regular,prices.premium,prices.diesel,places.state FROM places LEFT JOIN prices ON places.place_id = prices.prices_place_id")
@@ -22,9 +13,6 @@ try:
     places=[]
 
     for a in cursor:
-        #place={'name' : a[1],'cre_id' : a[2], 'x' : a[3], 'y' : a[4]}
-
-
         place = { 'type' : 'Feature' , 'geometry' : { 'type' : 'Point' , 'coordinates' : [ a[3] , a[4] ] }}
         properties={ 'name' : a[1] }
         if(a[5]!=None):
@@ -41,18 +29,13 @@ try:
 
     #f= open(PublicPATH+"data.json","w+")
     doc={ 'type' : 'FeatureCollection' , 'features' : places }
-    with open(PublicPATH+'js/data.json', 'w') as outfile:
+    with open('mezzanine-plugin/mezzanine_gasStation_map/static/'+'js/data.json', 'w') as outfile:
         json.dump(doc, outfile, indent=4, sort_keys=True)
-        print(PublicPATH+"js/data.json")
+        print('mezzanine-plugin/mezzanine_gasStation_map/static/js'+"js/data.json")
     #output = subprocess.run(["scp",PublicPATH+"data.json","quantics@132.247.186.67:public_html/static"])
 
     cnx.commit()
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+except psycopg2.Error as err:
+    print(err)
 else:
     cnx.close()
--- StoreDB_Postgresql.py
+++ StoreDB_Postgresql.py
@@ -1,51 +1,88 @@
+#!/usr/bin/env python3
+import glob, os
+import xml.etree.ElementTree as ET
+import numpy as np
 import json
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+from pprint import pprint
+from shapely.geometry import MultiPolygon, Polygon
+import shapely.geometry as sg
+import shapely.ops as so
 import subprocess
 import psycopg2
 
-p = open("routes.json")
-routes = json.load(p)
-ProyectPATH=routes['proyect']
-PublicPATH=routes['public']
-p.close()
+with open('mexicostatesprod.json') as statesgeojson:
+    states = json.load(statesgeojson)
 
-with open('db.json') as json_file:
-    config = json.load(json_file)
+allStates=[]
+for state in states['features']:
+    if(len(state['geometry']['coordinates'][0])>1):
+        Estado = Polygon([(i[0],i[1]) for i in state['geometry']['coordinates'][0]])
+
+    else:
+        Estado=MultiPolygon([Polygon([[(c[0],c[1]) for c in p] for p in m][0]) for m in state['geometry']['coordinates']])
+
+    allStates.append([Estado,state['properties']['postal']])
+
+
+def searchstate(x,y,allStates):
+    place=Point(y,x)
+    for state in allStates:
+        if(state[0].contains(Point(x,y))):
+            return(state[1])
 
 try:
     cnx = psycopg2.connect(os.getenv('DATABASE_URL', 'Optional default value'))
     cursor = cnx.cursor()
     updatestates = "INSERT INTO pricesTime(statec,number,datec,priceregular,pricepremium,pricediesel,nregular,npremium,ndiesel) SELECT tmp.state, tmp.numberplaces ,tmp. today, tmp.pricer, tmp.pricep, tmp.pricep, tmp.numberr, tmp.numberp, tmp.numberd FROM ( SELECT places.state AS state, COUNT(*) AS numberplaces, CURDATE() AS today, SUM(regular) AS pricer, SUM(premium) AS pricep, SUM(diesel) AS priced, COUNT(regular) AS numberr, COUNT(premium) AS numberp, COUNT(diesel) AS numberd FROM places LEFT JOIN prices ON places.place_id = prices.prices_place_id WHERE state is not null GROUP BY places.state ) AS tmp ON DUPLICATE KEY UPDATE priceregular = tmp.pricer, pricepremium = tmp.pricep, pricediesel = tmp.priced, nregular = tmp.numberr, npremium = tmp.numberp, ndiesel = tmp.numberd"
-    query=("SELECT places.place_id,places.name,places.cre_id,places.x,places.y,prices.regular,prices.premium,prices.diesel,places.state FROM places LEFT JOIN prices ON places.place_id = prices.prices_place_id")
     cursor.execute(updatestates)
-    cursor.execute(query)
-    places=[]
+    query1 = ("INSERT INTO places(place_id,name,cre_id,x,y,state) VALUES (%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE name = %s , cre_id = %s , x = %s , y = %s , state = %s ")
+    queryr = ("INSERT INTO prices( prices_place_id , regular ) VALUES ( %s , %s ) ON DUPLICATE KEY UPDATE regular = %s ")
+    queryp = ("INSERT INTO prices( prices_place_id , premium ) VALUES ( %s , %s ) ON DUPLICATE KEY UPDATE premium = %s ")
+    queryd = ("INSERT INTO prices( prices_place_id , diesel ) VALUES ( %s , %s ) ON DUPLICATE KEY UPDATE diesel = %s ")
 
-    for a in cursor:
-        #place={'name' : a[1],'cre_id' : a[2], 'x' : a[3], 'y' : a[4]}
+    os.chdir(ProyectPATH)
+    for file in glob.glob("*0.xml"):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for GasStation in root:
+
+            id = GasStation.get('place_id')
+            name = GasStation.find('name').text
+            cre_id = GasStation.find('cre_id').text
+            location = GasStation.find('location')
+            x = location.find('x').text
+            y = location.find('y').text
+            state = searchstate(float(x),float(y),allStates)
+            data_query = ( id , name , cre_id , x , y , state , name , cre_id , x , y , state)
+            cursor.execute(query1,data_query)
 
 
-        place = { 'type' : 'Feature' , 'geometry' : { 'type' : 'Point' , 'coordinates' : [ a[3] , a[4] ] }}
-        properties={ 'name' : a[1] }
-        if(a[5]!=None):
-            properties['regular']=a[5]
-        if(a[6]!=None):
-            properties['premium']=a[6]
-        if(a[7]!=None):
-            properties['diesel']=a[7]
-        if(a[8]!=None):
-            properties['state']=a[8]
-        place['properties']=properties
+    for file in glob.glob("*1.xml"):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for place in root:
+            id = place.get('place_id')
+            P = [0,0,0]
+            for gas_price in place.iter('gas_price'):
 
-        places.append(place)
+                type = gas_price.attrib['type']
+                price = gas_price.text
+                data_query2 = (id,price,price)
+                if(type == 'regular'):
+                    query2 = queryr
+                elif(type == 'premium'):
+                    query2 = queryp
+                else:
+                    query2 = queryd
 
-    #f= open(PublicPATH+"data.json","w+")
-    doc={ 'type' : 'FeatureCollection' , 'features' : places }
-    with open(PublicPATH+'js/data.json', 'w') as outfile:
-        json.dump(doc, outfile, indent=4, sort_keys=True)
-        print(PublicPATH+"js/data.json")
-    #output = subprocess.run(["scp",PublicPATH+"data.json","quantics@132.247.186.67:public_html/static"])
+                cursor.execute(query2,data_query2)
+
 
     cnx.commit()
+    subprocess.call("mv *.xml backup/", shell=True)
 
 except psycopg2.Error as err:
     print(err)
